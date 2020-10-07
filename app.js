530cdf04ab8212f491b8dceb1fac7d939d96dbb6
ha trieu kim <hatrieukim@brickmate.kr>
2020-10-07 09:42:53 +0700

    commit

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 
 
 
+mongoose.connect('mongodb://localhost:27017/freecodecamp')
 
 app.listen(3000, () => {
-    console.log('Server started hehe')
+    console.log('Server started hehe');
 })
